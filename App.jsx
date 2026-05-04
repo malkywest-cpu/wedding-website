dd6c0dfbb2843c80c78227ffe33a7a02ec1da0ab
malkywest-cpu <malkywest@me.com>
2026-05-04 13:39:34 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Our Wedding</h1>
+      <p>If you see this, React is working!</p>
+    </div>
+  );
+}
