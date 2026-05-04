a1baafdf0e489bc9368b57c608cd9d8b4087dc0e
malkywest-cpu <malkywest@me.com>
2026-05-04 14:00:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Our Wedding</h1>
+      <p>If you see this, React is working!</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
